1f59621cb88c02ec9951e31c7ee8e8ba2edfebe7
sanjay Anand <w0770698@myscc.ca>
2022-05-27 12:55:45 -0400

    changed the version in package json file

--- SanandMyFavouriteWebSeries/src/app/content-list/content-list.component.ts
+++ SanandMyFavouriteWebSeries/src/app/content-list/content-list.component.ts
@@ -16,16 +16,16 @@ seriesList: Content[];
       author:
         "Carter Bays-Craig Thomas",
       imagelink:"https://static.wikia.nocookie.net/himym/images/4/46/HIMYM_poster.jpg",
-      type:"Sitcom-Romace-Comedy",
-      hashtags:["#himym", "#legendary"] },
+      type:"Sitcom",
+      hashtags:["himym", "legendary"] },
       {
         id: 1,
         title:"Friends",
         body:"Ross Geller, Rachel Green, Monica Geller, Joey Tribbiani, Chandler Bing, and Phoebe Buffay are six twenty-somethings living in New York City. Over the course of 10 years and seasons, these friends go through life lessons, family, love, drama, friendship, and comedy.",
         author:"David Crane & Marta Kauffman",
         imagelink:"https://images.indianexpress.com/2021/05/friends-the-reunion-1200-2.jpg",
-        type:"Sitcom-Comedy",
-        hashtags: ["#friends", "#howyoudoin", "#illbethereforyou"]
+        type:"sitcom",
+        hashtags: ["friends", "howyoudoin", "illbethereforyou"]
       },
       {
         id: 2,
@@ -33,8 +33,8 @@ seriesList: Content[];
         body:"Comedy series following the exploits of Det. Jake Peralta and his diverse, lovable colleagues as they police the NYPD's 99th Precinct.",
         author:"Dan Goor- Michael Schur",
         imagelink:"https://m.media-amazon.com/images/M/MV5BNzVkYWY4NzYtMWFlZi00YzkwLThhZDItZjcxYTU4ZTMzMDZmXkEyXkFqcGdeQXVyODUxOTU0OTg@._V1_.jpg",
-        type:"funny",
-        hashtags:["#bnn", "#high"]
+        
+        hashtags:["bnn", "high"]
       },
       {
         id: 3,
@@ -42,8 +42,8 @@ seriesList: Content[];
         body:"Three different but related families face trials and tribulations in their own uniquely comedic ways.",
         author:"Steven Levitan- Christopher Lloyd",
         imagelink:"https://cdn1.edgedatg.com/aws/v2/abc/ModernFamily/showimages/cae29355a2f177539897e6db1d9b0861/1600x900-Q90_cae29355a2f177539897e6db1d9b0861.jpg",
-        type:"Comedy-family",
-        hashtags:["Modernfamily", "#lukedhunpy"]
+        
+        hashtags:["Modernfamily", "lukedhunpy"]
       },
       {
         id: 4,
@@ -51,13 +51,30 @@ seriesList: Content[];
         body:"A mockumentary on a group of typical office workers, where the workday consists of ego clashes, inappropriate behavior, and tedium..",
         author:"Greg Daniels",
         imagelink:"https://m.media-amazon.com/images/M/MV5BMDNkOTE4NDQtMTNmYi00MWE0LWE4ZTktYTc0NzhhNWIzNzJiXkEyXkFqcGdeQXVyMzQ2MDI5NjU@._V1_FMjpg_UX1000_.jpg",
-        type:"Sitcom",
-        hashtags:["#office", "#Michaelscott"]
+        type:"comedy",
+        hashtags:["office", "Michaelscott"]
+    },
+    {
+      id: 5,
+      title:"Breaking Bad",
+      body:"A high school chemistry teacher diagnosed with inoperable lung cancer turns to manufacturing and selling methamphetamine in order to secure his family's future.",
+      author:"Vince Gilligan",
+      imagelink:"https://ntvb.tmsimg.com/assets/p8696131_b_h10_aa.jpg?w=1280&h=720",
+      type:"crime",
+      hashtags:["WalterWhite", "JessePinkman"]
+    },{
+      id: 6,
+      title:"YOU",
+      body:"A dangerously charming, intensely obsessive young man goes to extreme measures to insert himself into the lives of those he is transfixed by.",
+      author:"Greg Berlanti",
+      imagelink:"https://m.media-amazon.com/images/M/MV5BMzkzOGFiY2EtZDcyZi00OWNhLThhNmYtYjQ3ODljMTM5ZDBkXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",
+      type:"crime",
+      hashtags:["JoeGoldberg", "LoveQuinn"]
     }];
 
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
   }
 
 }
